cf7851d20481cd6219b66bf8748fcadd5cac1e3d
Patricia Martínez Espert <patrimespert@outlook.com>
2025-01-29 22:01:56 +0100

    Adds files

--- app/src/main/java/com/example/jetpackcomposemasterclass/basic_layout/FlowLayout.kt
+++ app/src/main/java/com/example/jetpackcomposemasterclass/basic_layout/FlowLayout.kt
@@ -1,32 +1,61 @@
 package com.example.jetpackcomposemasterclass.basic_layout
 
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.FlowRowOverflow
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.AssistChip
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpackcomposemasterclass.ui.theme.JetpackComposeMasterclassTheme
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
-fun FlowLayoutDemo() {
-    Row(
+fun FlowLayoutDemo(modifier: Modifier = Modifier) {
+    FlowRow(
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxSize(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        maxLines = 4,
+        overflow = FlowRowOverflow.expandOrCollapseIndicator(
+            expandIndicator = {
+                IconButton(
+                    onClick = {}
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.KeyboardArrowDown,
+                        contentDescription = "Expand"
+                    )
+                }
+            },
+            collapseIndicator = {
+                IconButton(
+                    onClick = {}
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.KeyboardArrowUp,
+                        contentDescription = "Expand"
+                    )
+                }
+            }
+        )
     ) {
-        AssistChip(
-            onClick = {},
-            label = {
-                Text("Hello world")
-            }
-        )
-        AssistChip(
-            onClick = {},
-            label = {
-                Text("Hello world")
-            }
-        )
+        for(i in 1 ..30) {
+          AssistChip(
+              onClick = {},
+              label = {
+                  Text("Hello world")
+              }
+          )
+        }
     }
 }
 
